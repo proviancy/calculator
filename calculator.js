1bc4a96d5c6c43227ffcba5c38c9a076bfc51058
cooper <mccoy.cooperf@gmail.com>
2023-01-07 15:44:46 -0500

    Create operate function and HTML framework

--- calculator.js
+++ calculator.js
@@ -1,15 +1,33 @@
 function add(num1, num2) {
-    return num1 + num2;
+    answer = num1 + num2;
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    answer = num1 - num2;
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    answer = num1 * num2;
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    answer = num1 / num2;
+}
+
+let answer = 0;
+
+function operate(operation, num1, num2) {
+    if (operation === add){
+        add(num1,num2);
+        return answer;
+    } else if (operation === subtract) {
+        subtract(num1,num2);
+        return answer;
+    } else if (operation === multiply) {
+        multiply(num1,num2);
+        return answer;
+    } else if (operation === divide) {
+        divide(num1,num2);
+        return answer;
+    }
 }
